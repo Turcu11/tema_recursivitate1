2e44de0e23d75c0184ee9c85233e2c245ab672a2
Iosif Turcu <iosif.turcu@emmanuel.ro>
2021-12-25 21:09:39 +0200

    Running

--- Recursivitate/Program.cs
+++ Recursivitate/Program.cs
@@ -3,10 +3,30 @@
 namespace Recursivitate
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter the size of the array: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+
+            Console.WriteLine("Please fill in the array.");
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write($"Element of {i} = ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            PrintArrayWithRecursionMethod(array, 0, n);
+        }
+
+        public static void PrintArrayWithRecursionMethod(int[] array, int start, int end)
+        {
+            if(start >= end)
+            {
+                return;
+            }
+            Console.WriteLine($"Array = {array[start]}");
+            PrintArrayWithRecursionMethod(array, start + 1, end);
         }
     }
 }
